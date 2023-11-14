8e8c387ad806b6c328e4ea6f0662273ddbd3a6fc
younyoungwoong <rsm108416@gmail.com>
2023-11-14 14:21:31 +0900

    start

---
+++ src/main/java/com/mango/lolhero/LolheroApplication.java
@@ -0,0 +1,13 @@
+package com.mango.lolhero;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LolheroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LolheroApplication.class, args);
+	}
+
+}
